039aca00d880bba121db3f754b4f53de7aa57f15
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2014-10-29 00:15:14 +0100

    Añadido manejo de formularios

--- practica-webpy/css-exercise3.py
+++ practica-webpy/css-exercise3.py
@@ -1,19 +1,27 @@
 # -*- encoding: utf-8 -*-
 import web
-        
-# Asocia cualquier ruta url con la clase 'hola'
+
+# Asocia cualquier ruta url con la clase 'css'
 urls = (
-	'/(.*)', 'hola'  # Expresión regular, clase asociada
+	'/(.*)', 'css'  # Expresión regular, clase asociada
 )
 app = web.application(urls, globals())
 
-class hola:        
-	# name es el contenido del primer paréntesis    
+class css:
+	# name es el contenido del primer paréntesis
 	def GET(self, name):    # petición GET
 		if not name:  name = 'mundo'
-		return "<html> <head></head><body><h1>Hola!</h1></body>"
+		return '<html> \
+		<head>\
+		<link rel="stylesheet" type="text/css" href="./hoja-exercise3.css" media="screen"/>\
+		</head>\
+		<body>\
+		<h1>Mostrando imagenes estáticas con hojas de estilo</h1>\
+		<img href="./static/images/estatica1f.png">\
+		</body>'
 
-# Para que se ejecute como aplicación independiente 
+
+# Para que se ejecute como aplicación independiente
 # con el servidor de web propio
 if __name__ == "__main__":
 	app.run()
