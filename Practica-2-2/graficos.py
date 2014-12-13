a6f4edfd3e775657d92260fab4e9263fe16fe637
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2014-12-13 03:36:31 +0100

    Añadido el ejercicio de gráficos y el ejercicio de mapas

--- Practica-2-2/graficos.py
+++ Practica-2-2/graficos.py
@@ -11,42 +11,61 @@ urls = ('/', 'index')
 app = web.application(urls, globals())
 
 myform = form.Form(
-    form.Textbox('nombre',
-				description='Nombre de la serie', 
+	form.Textbox('nombre',
+				description='Nombre de la serie',
 				class_='form-control'),
-    form.Textbox('serie',
+	form.Textbox('serie',
 				description='Valores de la serie separados por comas',
-                 class_='form-control')
+				 class_='form-control')
 				)
 
 class index:
-    def GET(self):
-        form = myform()
-        # make sure you create a copy of the form by calling it (line above)
-        # Otherwise changes will appear globally
-        return render.formtest(form)
+	"""
+		Inserta una serie en la basde de datos con el formato 
+			{"nombre": <string>nombre,
+			"serie": [<int>,<int>,<int>,...]}
+		@param nombre String del nombre
+		@param numeroSerie Lista de int que forman la serie
+	"""
+	def insertarSerie(self, nombre, numerosSerie):
+		serie = {"nombre": nombre,
+				"serie": numerosSerie}
+		seriesCol = db.series
+		print ("Insertada la serie" + str(serie))
+		return seriesCol.insert(serie)
+	
+	def getSerieAll(self):
+		series = db.series.find()
+		seriesLista = []
+		for serie in series:
+			seriesLista.append({"data" : serie["serie"],"name" : str(serie["nombre"])})
+		return seriesLista;	
+	
+	def borrarSeries(self):
+		db.series.remove()
+		
+	def GET(self):
+			
+		form = myform()
+		# make sure you create a copy of the form by calling it (line above)
+		# Otherwise changes will appear globally
+		seriesLista = self.getSerieAll();
+		return render.grafico(form, seriesLista)
 
-    def POST(self):
-        form = myform()
-        if not form.validates():
-            return render.formtest(form)
-        else:
-            # form.d.boe and form['boe'].value are equivalent ways of
-            # extracting the validated arguments from the form.
-            numerosSerie=str(form['serie'].value);
-            numerosSerie = [int(x) for x in numerosSerie.split(',')]
-            print numerosSerie
-            serie = {"nombre": form['nombre'].value,
-					"serie": numerosSerie}
-            series = db.series
-            idSerie = series.insert(serie);
-            
-            if(idSerie):
-            	serieBD = series.find_one({'_id': idSerie})
-            	return str(serieBD["serie"])
-            else:
-            	return "Los valores no han sido guardados"
-
+	def POST(self):
+ 		form = myform()
+		form.validates()
+		
+		nombre = str(form["nombre"].value);
+		numerosSerie=str(form["serie"].value);
+		numerosSerie = [int(x) for x in numerosSerie.split(',')]
+		
+		self.insertarSerie(nombre,numerosSerie)
+		seriesLista = self.getSerieAll();
+		
+		return render.grafico(form, seriesLista)
+	
+	
 if __name__ == "__main__":
-    web.internalerror = web.debugerror
-    app.run()
+	web.internalerror = web.debugerror
+	app.run()
