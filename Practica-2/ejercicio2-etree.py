945dcf59847ebe377ffb099e507f3465dd9692a3
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2015-01-13 10:26:11 +0100

    Subida de lapractica 4, con todo funcionando

--- Practica-2/ejercicio2-etree.py
+++ Practica-2/ejercicio2-etree.py
@@ -1,32 +1,49 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 2/12/2014
 
 @author: ivanortegaalba
 '''
-# -*- coding: utf-8 -*-
+
 
 from lxml import etree
+import re
+import urllib
+import sys
 
-tree = etree.parse('portada.xml')
+im = re.compile('image(.?)*')
 
-rss = tree.getroot() # elemento raiz
+class ParseRssNews():
+	contN = 0
+	contI = 0
+	if len(sys.argv) > 1:
+		encontrado = False
+		termino = sys.argv[2]
 
-# Los elementos funcionan como listas
-channel = rss[0]     # Primer hijo
+	def start (self, tag, attrib): # Etiquetas de inicio
+		if tag == 'item':
+			self.contN += 1
+		if tag == 'enclosure':
+			for k in attrib:
+				if k == 'type' and im.search(attrib[k]) != None:
+					self.contI += 1
+					name = 'imagen' + str(self.contI) + '.jpg'
+					#urllib.urlretrieve(attrib['url'], "img/"+name);
 
-for e in  channel:
-	if (e.tag == 'item'):  # Atributo tag, nobre del elemento
-		# Anado <item modificado="hoy">
-		e.set('modificado', 'hoy')		
-		
-		# Los atributos del xml funcionan casi como diccionarios		
-		print (e.keys(), e.get('modificado'))		
-		
-		# Creo otro elemento
-		otro = etree.Element('otro')   # <otro></otro>
-		
-		# Atributo text
-		otro.text = 'Texto de otro'
-		e.insert(0, otro) # Anado <otro>Texto de otro</otro>
+	def data (self, data):
+		if len(sys.argv) > 1:
+			tr = re.compile('\s'+self.termino+'\s')
+			if tr.search(data) != None:
+				self.encontrado = True
 
-print (etree.tounicode(rss, pretty_print=True))
+	def close (self):
+		print 'Número de noticias: ' + str(self.contN)
+		print 'Número de imágenes: ' + str(self.contI)
+		if len(sys.argv) > 1:
+			if self.encontrado:
+				print 'El termino ' +self.termino+ ' está en el documento.'
+			else:
+				print 'El termino ' +self.termino+ ' no está en el documento.'
+
+parser = etree.XMLParser (target=ParseRssNews())
+etree.parse (sys.argv[1], parser)
